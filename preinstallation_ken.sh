22d5d1de7f1074dcfa820799c03c51811b04e313
KenHuang <kenhuang2019iii@gmail.com>
2020-10-15 23:53:10 +0800

    add a condition of connection for git and github

--- preinstallation_ken.sh
+++ preinstallation_ken.sh
@@ -5,40 +5,42 @@
 
 # download package information from all configured sources
 
-echo "===================================================== Switch to root ====================================================="
-sudo -i
+echo "===================================================== Updating apt... ====================================================="
+sudo apt-get update -y
 
-echo "=== Updating apt... ==="
-apt-get update -y
+echo "===================================================== Installing git... ====================================================="
+sudo apt-get install git -y
 
-echo "=== Installing git... ==="
-apt-get install git -y
+echo "Do you wnat to connect Git to Github? [y/n]"
+read CONNECT_WISH;
+if [[ $CONNECT_WISH == "y" || $CONNECT_WISH == "Y" ]]
+then
+    # Connect git to github
+    echo "===================================================== Configuring git... ====================================================="
 
-# Connect git to github
-echo "=== Configuring git... ==="
+    echo "Please enter github username:"
+    read GITHUB_USER;
+    git config --global user.name "${GITHUB_USER}"
 
-echo "Please enter github username:"
-read GITHUB_USER;
-git config --global user.name "${GITHUB_USER}"
+    echo "Please enter github E-maill:"
+    read GITHUB_EMAIL;
+    git config --global user.email "${GITHUB_EMAIL}"
 
-echo "Please enter github E-maill:"
-read GITHUB_EMAIL;
-git config --global user.email "${GITHUB_EMAIL}"
+    echo "===================================================== Git configuraiton done. ====================================================="
+    git config --list
+fi
 
-echo "=== Git configuraiton done. ==="
-git config --list
+echo "===================================================== Installing python3 ====================================================="
+sudo apt-get install python3 -y
 
-echo "=== Installing python3 ==="
-apt-get install python3 -y
+echo "===================================================== Installing python3-opencv ====================================================="
+sudo apt-get install python3-opencv -y
 
-echo "=== Installing python3-opencv ==="
-apt-get install python3-opencv -y
+echo "===================================================== Installing p7zip-full ====================================================="
+sudo apt-get install p7zip-full -y
 
-echo "=== Installing p7zip-full  ==="
-apt-get install p7zip-full -y
+echo "===================================================== Installing dkms ====================================================="
+sudo apt-get install dkms -y
 
-echo "=== Installing dkms ==="
-apt-get install dkms -y
-
-echo "=== Installing curl ==="
-apt-get install curl -y
+echo "===================================================== Installing curl ====================================================="
+sudo apt-get install curl -y
